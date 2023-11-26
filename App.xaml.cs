371601edfc6cc1438e0973ab8b321404bcbd2e7c
User <@DESKTOP-543533>
2023-11-26 22:43:10 +0300

    Добавила

---
+++ App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using AnastasiaSoboleva.Entities;
+using AnastasiaSoboleva.Properties;
+
+namespace AnastasiaSoboleva
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities.SobolevaEntities Context { get; set; } = new SobolevaEntities();
+        public static User AuthUser
+        {
+            get
+            {
+                return Context.User.Find(Settings.Default.UserID);
+            }
+        }
+
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show("Произошла непредвиденная ошибка в работе программы, проверьте подключение к базе данных. Если ошибка появится снова - обратитесь к вашему системному администратору.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information); ;
+        }
+    }
+}
